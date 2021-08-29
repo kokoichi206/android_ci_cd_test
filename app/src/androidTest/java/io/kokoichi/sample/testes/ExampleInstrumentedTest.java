8b418bc517c3241d39c625714cf4e993bffc795d
kokoichi <tt.300607@gmail.com>
2021-08-29 09:59:49 +0900

    ee

--- app/src/androidTest/java/io/kokoichi/sample/testes/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/kokoichi/sample/testes/ExampleInstrumentedTest.java
@@ -9,7 +9,9 @@ import android.content.Context;
 import androidx.constraintlayout.utils.widget.MockView;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -22,6 +24,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
--- app/src/main/java/io/kokoichi/sample/testes/MainActivity.java
+++ app/src/main/java/io/kokoichi/sample/testes/MainActivity.java
@@ -1,14 +1,10 @@
 package io.kokoichi.sample.testes;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.jarjarred.org.antlr.v4.tool.Rule;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
